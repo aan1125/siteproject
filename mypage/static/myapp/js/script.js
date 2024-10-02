e29ba48c8e469c7dc2dc9384caa731dca70f697c
todnsns079@naver.com <112530053+aan1125@users.noreply.github.com>
2024-10-02 20:40:13 +0900

    첫업로드
    첫업로드

---
+++ mypage/static/myapp/js/script.js
@@ -0,0 +1,5 @@
+function redirectToPage() {
+    // 이동할 페이지의 URL을 입력하세요
+    window.location.href = 'register.html';
+    
+  }
---
+++ mypage/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'myapp/index.html')
+def register(request):
+    return render(request, 'myapp/register.html')
